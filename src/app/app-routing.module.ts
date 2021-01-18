21891cf024f9a11c9fb8f8e557662792c21b673a
Superxwolf <Superxwolf@hotmail.com>
2021-01-18 18:27:37 -0400

    Added toolbar from old project. Fixed up the css. Added Home and CreateCharacter with basic text. Added images from old project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateCharacterComponent } from './create-character/create-character.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+	{ path: '', component: HomeComponent },
+	{ path: 'create-character', component: CreateCharacterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
