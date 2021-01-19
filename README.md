19956b1268aa995d73d7864907f83674c43de6fa
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2021-01-18 20:53:36 -0400

    NON-FUNCTIONAL- TOOLBAR MENU ADDED
    Co-Authored-By: Superxwolf <Superxwolf@hotmail.com>

--- README.md
+++ README.md
@@ -8,12 +8,11 @@ This was a modification of Pathfinder-character-manager and given a small overha
 
 ## To Use
 
-To clone and run this repository you'll need [Git](https://git-scm.com), [Bower](https://bower.io/) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+To clone and run this repository you'll need [Git](https://git-scm.com), and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
 
 ```bash
 # Install dependencies
 npm install
-bower install
 # Run the app
 npm start
 ```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CreateCharacterComponent } from './create-character/create-character.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu'; 
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { CreateCharacterComponent } from './create-character/create-character.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
