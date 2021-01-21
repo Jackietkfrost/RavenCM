0263a25038bcb3b9e78e891e42f0a2e234360a66
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2021-01-20 20:27:55 -0400

    Added new modules for character build section, moved files around and obsoleted create-character for now.
    Co-Authored-By: Superxwolf <Superxwolf@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CreateCharacterComponent } from './create-character/create-character.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RaceComponent } from './menu-build/race/race.component';
+import { ClassComponent } from './menu-build/class/class.component';
+import { BackgroundComponent } from './menu-build/background/background.component';
+import { AbilityScoresComponent } from './menu-build/ability-scores/ability-scores.component';
+import { LanguagesComponent } from './menu-build/languages/languages.component';
+import { ProficienciesComponent } from './menu-build/proficiencies/proficiencies.component';
+import { FeatsComponent } from './menu-build/feats/feats.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CreateCharacterComponent
+    CreateCharacterComponent,
+    RaceComponent,
+    ClassComponent,
+    BackgroundComponent,
+    AbilityScoresComponent,
+    LanguagesComponent,
+    ProficienciesComponent,
+    FeatsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu-build/build-submenu/build-submenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-build-submenu',
+  templateUrl: './build-submenu.component.html',
+  styleUrls: ['./build-submenu.component.scss']
+})
+export class BuildSubmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
