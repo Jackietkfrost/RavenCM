592a75e2a5b3cdaacd1d9c5c6aec30a4aa9a6add
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-05-23 18:32:23 -0400

    Update BuildScreen.vue
    Added in app bar. Needs rest of work.

--- src/renderer/screens/BuildScreen.vue
+++ src/renderer/screens/BuildScreen.vue
@@ -1,11 +1,13 @@
-<script setup lang="tsx">
-import { useI18n } from 'vue-i18n'
-import { mdiEmoticonCoolOutline } from '@mdi/js'
-
-const { t } = useI18n()
-</script>
-
 <template>
+  <v-app-bar color="primary" density="compact" height="50">
+    <v-btn>Race</v-btn>
+    <v-btn>Class</v-btn>
+    <v-btn>Background</v-btn>
+    <v-btn>Ability Scores</v-btn>
+    <v-btn>Languages</v-btn>
+    <v-btn>Proficiency</v-btn>
+    <v-btn>Feats</v-btn>
+  </v-app-bar>
   <v-container>
     <v-row no-gutters class="text-center">
       <v-col cols="12">
@@ -15,3 +17,10 @@ const { t } = useI18n()
     </v-row>
   </v-container>
 </template>
+
+<script setup lang="tsx">
+import { useI18n } from 'vue-i18n'
+import { mdiEmoticonCoolOutline } from '@mdi/js'
+
+const { t } = useI18n()
+</script>
