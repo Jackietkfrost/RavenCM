16903a88ccb9df1ae8d4af3615f973be125f3e74
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-05-23 14:58:07 -0400

    Update CharacterSheet.vue

--- src/renderer/screens/CharacterSheet.vue
+++ src/renderer/screens/CharacterSheet.vue
@@ -1,17 +1,18 @@
 <script setup lang="tsx">
-import { useI18n } from 'vue-i18n'
-import { mdiEmoticonCoolOutline } from '@mdi/js'
+// import { useI18n } from 'vue-i18n'
+import { mdiFile } from '@mdi/js'
 
-const { t } = useI18n()
+// const { t } = useI18n()
 </script>
 
 <template>
   <v-container>
+    <v-row no-gutters class="text-center">Generate preview of pdf sheet</v-row>
     <v-row no-gutters class="text-center">
       <v-col cols="12">
-        <v-icon :icon="mdiEmoticonCoolOutline" size="250" color="#009f57" />
+        <v-icon :icon="mdiFile" size="250" color="#009f57" />
       </v-col>
-      <v-col cols="12" class="my-4">{{ t('desc.second-desc') }}</v-col>
+      <v-col cols="12" class="my-4">PDF PREVIEW HERE</v-col>
     </v-row>
   </v-container>
 </template>
