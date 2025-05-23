a0b9c6e134a12f13f97ba070d64d807aefc1123b
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-05-23 15:14:27 -0400

    Create AdditionalContent.vue
    added skeleton for additional content page

---
+++ src/renderer/screens/startScreens/AdditionalContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Extending the content</h1>
+  <p>
+    Raven Character Manager comes bundled with all content that is included in the System Reference
+    Document released under the Open Gaming License. To extend the content, you can build your own
+    XML files and add them to the content folder, or use an index file.
+  </p>
+  <h1>Index Files</h1>
+  <p>
+    Index files allow the creator to update his or her files and allow Aurora to automatically
+    download and update the content When you've obtained the URL of the .index file; enter it in the
+    field below and click 'Download' and then click 'Update Content Files'.
+  </p>
+</template>
+<script setup lang="tsx"></script>
+<style></style>
