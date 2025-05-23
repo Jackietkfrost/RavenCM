5f50c09656bbb3f5d7f8d124ca91cd540e4fb722
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-05-23 18:33:32 -0400

    Update IPCs.ts
    Added in call for saving a character.

--- src/main/IPCs.ts
+++ src/main/IPCs.ts
@@ -1,4 +1,6 @@
 import { ipcMain, shell, IpcMainEvent, dialog } from 'electron'
+import * as fs from 'fs'
+import * as path from 'path'
 import Constants from './utils/Constants'
 
 /*
@@ -29,6 +31,51 @@ export default class IPCs {
         filters
       })
       return dialogResult
+    })
+
+    ipcMain.handle('msgSaveCharacter', async (event: IpcMainEvent, data: any) => {
+      console.log(data)
+      const characterFolder = 'GameCharacters'
+      const fileName = `${data.characterName}.DnD5e`
+
+      // Create the GameCharacters folder if it doesn't exist
+      if (!fs.existsSync(characterFolder)) {
+        fs.mkdirSync(characterFolder)
+      }
+
+      // Create the XML file
+      const filePath = path.join(characterFolder, fileName)
+      const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+  <character version="1.0.0">
+  <!-- information -->
+	<information>
+		<group>${data.group}</group>
+		<generationOption>${data.generationOption}</generationOption>
+	</information>
+	<!-- display data -->
+	<display-properties favorite="true">
+    <name>${data.characterName}</name>
+    <race>${data.race}</race>
+		<class>${data.class}</class>
+		<archetype>${data.archetype}
+		</archetype>
+		<background>${data.background}</background>
+		<level>${data.level}</level>
+
+    <abilityGenerationOption>${data.abilityGenerationOption}</abilityGenerationOption>
+  </display-properties>
+  <!-- build data -->
+  <build>
+  <name>${data.characterName}</name>
+    <input>
+      <gender>${data.pronouns}</gender>
+      <player-name>${data.playerName}</player-name>
+			<experience>${data.characterExperience}</experience>
+  </character>`
+
+      fs.writeFileSync(filePath, xmlContent)
+
+      console.log(`Character file saved to ${filePath}`)
     })
   }
 }
