8012fad4cc7c56c2db5c39ad83d761ff0192ad01
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-05-24 23:48:40 -0400

    Update AbilityScoreScreen.vue
    Added in score systems. Missing a bit more.

--- src/renderer/screens/builderScreens/AbilityScoreScreen.vue
+++ src/renderer/screens/builderScreens/AbilityScoreScreen.vue
@@ -1,22 +1,19 @@
 <template>
-  <v-container
-    >Ability Scores
-    <v-number-input
-      :reverse="false"
-      controlVariant="stacked"
-      label="test"
-      :inset="false"
-      variant="solo"
-      flat
-      :max="20"
-      :min="3"
-      max-width="150"
-      tile
-    ></v-number-input>
+  <v-container>
+    <v-row>
+      <v-col cols="2"><AbilityScore label="Strength" />+0</v-col>
+      <v-col cols="2"><AbilityScore label="Dexterity" />+0</v-col>
+      <v-col cols="2"><AbilityScore label="Constitution" />+0</v-col>
+      <v-col cols="2"><AbilityScore label="Intelligence" />+0</v-col>
+      <v-col cols="2"><AbilityScore label="Wisdom" />+0</v-col>
+      <v-col cols="2"><AbilityScore label="Charisma" />+0</v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script setup lang="tsx">
+import AbilityScore from '@/renderer/components/ScoreSystem.vue'
 // import { useI18n } from 'vue-i18n'
+
 // const { t } = useI18n()
 </script>
