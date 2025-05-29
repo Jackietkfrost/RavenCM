7fdeb3b07053f1a3e8b0a9edc8391d86b9ca6bad
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-05-28 22:11:49 -0400

    Update RaceScreen.vue
    Fixed up some details, still needs more work. Threw expansion panels out the window because they were annoying me. (No way of receiving if panel is expanded at first glance, wasn't gonna look deep on something that's just a prototype for now.)

--- src/renderer/screens/builderScreens/RaceScreen.vue
+++ src/renderer/screens/builderScreens/RaceScreen.vue
@@ -15,40 +15,74 @@
         </v-btn>
       </v-col>
     </v-row>
-    <v-expansion-panels>
-      <v-expansion-panel>
-        <v-expansion-panel-title>
-          <v-row no-gutters>
-            <v-col class="d-flex justify-start" cols="4"> {{ t('BuildScreen.race') }} </v-col>
-            <v-col class="text--secondary" cols="8">
-              <v-fade-transition leave-absolute>
-                <v-row style="width: 100%" no-gutters>
-                  <v-col class="d-flex justify-start" cols="6">
-                    {{ characterStore.character?.race ?? '' }}
-                  </v-col>
-                </v-row>
-              </v-fade-transition>
-            </v-col>
-          </v-row>
-        </v-expansion-panel-title>
-        <v-data-table-virtual :headers="headers" :items="items"> </v-data-table-virtual>
-      </v-expansion-panel>
-    </v-expansion-panels>
+    <v-card>
+      <v-card-title>
+        <v-row no-gutters @click="() => (isExpanded = !isExpanded)">
+          <v-col class="" cols="4"> {{ t('BuildScreen.race') }} </v-col>
+          <v-col class="text--secondary" cols="6">
+            <v-text-field
+              readonly
+              class="race-box cursor-pointer"
+              v-model="textFieldValue"
+              variant="plain"
+              density="compact"
+              clearable
+              single-line
+              persistent-clear
+              hide-details
+              :dirty="characterStore.character.race ? characterStore.character.race.length > 0 : false"
+              @click:clear="onClear"
+            ></v-text-field>
+          </v-col>
+          <v-col class="d-flex justify-end" cols="2">
+            <v-icon class="ml-auto" @click="() => (isExpanded = !isExpanded)">
+              {{ isExpanded ? mdiChevronUp : mdiChevronDown }}
+            </v-icon>
+          </v-col>
+        </v-row>
+      </v-card-title>
+      <v-data-table-virtual
+        v-if="isExpanded"
+        :headers="headers"
+        :items="items"
+        :item-value="(item) => item.id"
+        hover
+        @dblclick:row="handleDoubleClick"
+      >
+      </v-data-table-virtual>
+    </v-card>
   </v-container>
 </template>
 
 <script setup lang="tsx">
 import { useAppStore } from '@/renderer/store/appStore'
-import { mdiFilterMenuOutline, mdiMagnify } from '@mdi/js'
+import { mdiChevronDown, mdiChevronUp, mdiFilterMenuOutline, mdiMagnify } from '@mdi/js'
 import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 const characterStore = useAppStore()
+const isExpanded = ref(true)
+const textFieldValue = ref(characterStore.character.race ? characterStore.character.race : '')
 const races = characterStore.races
 const headers = ref([
   { title: 'Name', key: 'name', align: 'start' as const },
   { title: 'Source', key: 'source', align: 'end' as const }
 ])
 const items = ref(races)
+
+const handleDoubleClick = (event: any, { item }: any) => {
+  characterStore.character.race = item.name
+  textFieldValue.value = item.name
+  isExpanded.value = !isExpanded.value
+}
+
+const onClear = () => {
+  characterStore.character.race = ''
+}
 </script>
+<style scoped>
+.race-box {
+  cursor: pointer;
+}
+</style>
